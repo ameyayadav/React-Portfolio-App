e6546e92f34cf315576399f8591e1234baba31e0
Ameya Yadav <ameyayadav2@gmail.com>
2023-03-19 23:59:35 +0000

    added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Header from "./components/Header";
-import Projects from "./pages/Projects";
-import Contact from "./pages/Contact";
-import Notfound from "./pages/Error";
-import Footer from "./components/Footer";
+import Home from "../src/pages/Home"
+import Header from "../src/Components/Header";
+import Projects from "../src/pages/Project";
+import Contact from "../src/pages/Contact";
+import Error from "../src/pages/Error";
+import Footer from "../src/Components/Footer";
 
 function App() {
   return (
@@ -17,7 +17,7 @@ function App() {
               <Route path="/" element={<Home />}></Route>
               <Route path="projects" element={<Projects />}></Route>
               <Route path="contact" element={<Contact />}></Route>
-              <Route path="*" extact={true} element={<Notfound />}></Route>
+              <Route path="*" extact={true} element={<Error />}></Route>
             </Routes>
             
             <Footer />
