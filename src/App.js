efef7e6a23326b8ae297a5a1555707f4ce769bd5
Ameya Yadav <ameyayadav2@gmail.com>
2023-03-19 22:25:28 +0000

    updated

--- src/App.js
+++ src/App.js
@@ -1,56 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-import { Route, Routes } from 'react-router-dom'
-// import Home from './components/Home'
-// import About from './Components/About'
-// import Contact from './Components/Contact'
-// import Layout from './components/Layout'
-// import Portfolio from './Components/Portfolio'
-// import Dashboard from './Components/Dashboard'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Header from "./components/Header";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import Notfound from "./pages/Error";
+import Footer from "./components/Footer";
 
-import Home from "./Components/Home/index";
-import Layout from './Components/Layout';
-import About from "./Components/About/about";
-import Contact from "./Components/Contact/contact";
-import Portfolio from "./Components/Portfolio/index";
-import Dashboard from "./Components/Dashboard/index";
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Route>
-      </Routes>
-    </>
-  )
+    <Router>
+        <div className="page-body">
+            <Header />
+
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="projects" element={<Projects />}></Route>
+              <Route path="contact" element={<Contact />}></Route>
+              <Route path="*" extact={true} element={<Notfound />}></Route>
+            </Routes>
+            
+            <Footer />
+        </div>
+    </Router>
+    
+  );
 }
 
-export default App
+export default App;
